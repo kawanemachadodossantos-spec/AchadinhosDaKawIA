18f4561f3dfe7bdf216da3f150598e50541a3948
kawanemachadodossantos-spec <kawanemachadodossantos@gmail.com>
2026-08-12 13:02:19 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import time
 import requests
 from dotenv import load_dotenv
+
 from src.buscador import buscar_ofertas_mercadolivre, buscar_ofertas_shopee
 
 # Adiciona o diretório atual e a pasta 'src' ao caminho de busca do Python
